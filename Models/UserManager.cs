1b445983f80ce2e93fd1c9da8bcfe2771c071045
Inshadh <inshadh.cypheron@gmail.com>
2025-01-30 13:54:33 +0530

    initial commit

---
+++ Models/UserManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.Models
+{
+    public class UserManager
+    {
+        public static ObservableCollection<User> _DatabaseUsers = new ObservableCollection<User>() { 
+        new User{Name="Nikhil Jose",Email="nikhilpj98@gmail.com" } };
+
+        public static ObservableCollection<User> GetUsers()
+        {
+            return _DatabaseUsers;
+        }
+
+        public static void AddUser(User user)
+        {
+            _DatabaseUsers.Add(user);
+        }
+    }
+}
